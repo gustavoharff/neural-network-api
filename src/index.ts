3aab35d9dcbd1430cfbb6e3473f9fc160e20e165
Gustavo Harff <gustavo.harff@gmail.com>
2022-09-29 15:18:11 -0300

    Add try catch

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,46 @@
-import express from 'express'
-import fileUpload from 'express-fileupload'
+import express from "express";
+import fileUpload from "express-fileupload";
 import Jimp from "jimp";
 
-import { net } from './net'
+import { net } from "./net";
 
-const app = express()
+const app = express();
 
 app.use(fileUpload());
 
-app.post('/image-recognition', async (request, response) => {
-  const files = request.files
+app.post("/image-recognition", async (request, response) => {
+  try {
+    const files = request.files;
 
-  if (!files) {
-    return response.json({ message: 'Nenhum imagem anexada.'})
+    if (!files) {
+      return response.json({ message: "Nenhum imagem anexada." });
+    }
+
+    if (files.file instanceof Array) {
+      return response.json({ message: "Insira apenas uma image." });
+    }
+
+    const file = await Jimp.read(files.file.data);
+
+    const value: number[] = [];
+
+    file.resize(10, 10).bitmap.data.forEach((number) => {
+      value.push(number);
+    });
+
+    const result = net.run(value);
+
+    return response.json(result);
+  } catch (error) {
+    console.log(error);
+    return response.status(400).json({ message: "Internal server error." });
   }
-
-  if (files.file instanceof Array) {
-    return response.json({ message: 'Insira apenas uma image.'})
-  }
-
-  const file = await Jimp.read(files.file.data)
-
-  const value: number[] = []
-
-  file.resize(10, 10).bitmap.data.forEach(number => {
-    value.push(number)
-  })
-
-  const result = net.run(value)
-
-  return response.json(result)
-})
+});
 
 app.listen(80, () => {
-  console.log('Running at 80')
-})
+  console.log("Running at 80");
+});
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ', err);
+process.on("uncaughtException", function (err) {
+  console.log("Caught exception: ", err);
 });
